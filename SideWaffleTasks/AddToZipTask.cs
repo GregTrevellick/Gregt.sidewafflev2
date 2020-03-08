f477eb964d0f4f077e11dacb9f1848d63f446475
Greg Trevellick <gtrevellick@googlemail.com>
2020-03-08 20:49:14 +0000

    .

--- SideWaffleTasks/AddToZipTask.cs
+++ SideWaffleTasks/AddToZipTask.cs
@@ -1,99 +1,101 @@
-﻿using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
-using System.IO.Compression;
+﻿//commented out as nothing uses this class and it doesn't compile
 
-namespace SideWaffleTasks
-{
-    public class AddToZipTask : Task {
-        public string ZipFilePath { get; set; }
-        public ITaskItem[] FilesToAdd { get; set; }
-        public string RootFolder { get; set; }
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.IO;
+//using System.IO.Compression;
+//using Microsoft.Build.Framework;
+//using Microsoft.Build.Utilities;
 
-        public string Compression { get; set; } = System.IO.Compression.CompressionLevel.Optimal.ToString();
+//namespace SideWaffleTasks
+//{
+//    public class AddToZipTask : Task {
+//        public string ZipFilePath { get; set; }
+//        public ITaskItem[] FilesToAdd { get; set; }
+//        public string RootFolder { get; set; }
 
-        public override bool Execute() {
-            Log.LogMessage(MessageImportance.High, "AddToZipTask-asm called", null);
+//        public string Compression { get; set; } = System.IO.Compression.CompressionLevel.Optimal.ToString();
 
-            CompressionLevel level = CompressionLevel.Optimal;
-            if (!string.IsNullOrWhiteSpace(Compression)) {
-                try {
-                    CompressionLevel result = (CompressionLevel)Enum.Parse(typeof(CompressionLevel), Compression);
-                    level = result;
-                }
-                catch (Exception ex) {
-                    Log.LogWarning("Unable to parse compression level [{0}]. Error [{1}]", Compression, ex.ToString());
-                }
-            }
+//        public override bool Execute() {
+//            Log.LogMessage(MessageImportance.High, "AddToZipTask-asm called", null);
 
-            string parentDir = Path.GetDirectoryName(ZipFilePath);
-            if (!Directory.Exists(parentDir)) {
-                Directory.CreateDirectory(parentDir);
-            }
-            // System.Diagnostics.Debugger.Launch();
-            try{
-                using (ZipArchive zip = ZipFile.Open(ZipFilePath, System.IO.Compression.ZipArchiveMode.Update)) {
-                    Uri rootFolderUri = new Uri(RootFolder);
-                    // add each input file to the zip
-                    foreach (var file in FilesToAdd) {
-                        // get the relative path of the file to to add
-                        string filePath = file.GetMetadata("FullPath");
-                        Uri fileUri = new Uri(filePath);
-                        string relpath = Uri.UnescapeDataString(
-                                                rootFolderUri.MakeRelativeUri(fileUri)
-                                                    .ToString()
-                                                    .Replace('/', Path.DirectorySeparatorChar)
-                                                );
+//            CompressionLevel level = CompressionLevel.Optimal;
+//            if (!string.IsNullOrWhiteSpace(Compression)) {
+//                try {
+//                    CompressionLevel result = (CompressionLevel)Enum.Parse(typeof(CompressionLevel), Compression);
+//                    level = result;
+//                }
+//                catch (Exception ex) {
+//                    Log.LogWarning("Unable to parse compression level [{0}]. Error [{1}]", Compression, ex.ToString());
+//                }
+//            }
 
-                        Log.LogMessage("Update zip: [{0}], add file: [{1}], relpath: [{2}]", ZipFilePath, filePath, relpath);
+//            string parentDir = Path.GetDirectoryName(ZipFilePath);
+//            if (!Directory.Exists(parentDir)) {
+//                Directory.CreateDirectory(parentDir);
+//            }
+//            // System.Diagnostics.Debugger.Launch();
+//            try{
+//                using (ZipArchive zip = ZipFile.Open(ZipFilePath, System.IO.Compression.ZipArchiveMode.Update)) {
+//                    Uri rootFolderUri = new Uri(RootFolder);
+//                    // add each input file to the zip
+//                    foreach (var file in FilesToAdd) {
+//                        // get the relative path of the file to to add
+//                        string filePath = file.GetMetadata("FullPath");
+//                        Uri fileUri = new Uri(filePath);
+//                        string relpath = Uri.UnescapeDataString(
+//                                                rootFolderUri.MakeRelativeUri(fileUri)
+//                                                    .ToString()
+//                                                    .Replace('/', Path.DirectorySeparatorChar)
+//                                                );
 
-                        // var entriesToDelete = new List<ZipArchiveEntry>();
-                        // if the file is already in the zip remove it and add again                        
-                        if (zip.Entries != null && zip.Entries.Count > 0) {
-                            List<ZipArchiveEntry> entries = zip.Entries.ToList();
-                            foreach (var entry in entries) {
-                                if (entry.FullName.Equals(relpath, StringComparison.OrdinalIgnoreCase)) {
-                                    // entriesToDelete.Add(entry);
-                                    Log.LogMessage(MessageImportance.Low, "deleting zip entry for [{0}]", relpath);
-                                    entry.Delete();
-                                }
-                            }
-                        }
-                        //if(entriesToDelete != null && entriesToDelete.Count > 0){
-                        //    foreach(var entry in entriesToDelete) {
-                        //        try {
-                        //            entry.Delete();
-                        //        }
-                        //        catch(Exception ex){
-                        //            Log.LogMessage(MessageImportance.Low, "Unable to delete entry from zip. {0}", ex.ToString());
-                        //        }
-                        //    }
-                        //}
-                        ZipFileExtensions.CreateEntryFromFile(zip, filePath, relpath, level);
-                    }
-                }
-            }
-            catch(Exception ex){
-                Log.LogError(ex.ToString());
-                return false;
-            }
+//                        Log.LogMessage("Update zip: [{0}], add file: [{1}], relpath: [{2}]", ZipFilePath, filePath, relpath);
 
-            return true;
-        }
+//                        // var entriesToDelete = new List<ZipArchiveEntry>();
+//                        // if the file is already in the zip remove it and add again                        
+//                        if (zip.Entries != null && zip.Entries.Count > 0) {
+//                            List<ZipArchiveEntry> entries = zip.Entries.ToList();
+//                            foreach (var entry in entries) {
+//                                if (entry.FullName.Equals(relpath, StringComparison.OrdinalIgnoreCase)) {
+//                                    // entriesToDelete.Add(entry);
+//                                    Log.LogMessage(MessageImportance.Low, "deleting zip entry for [{0}]", relpath);
+//                                    entry.Delete();
+//                                }
+//                            }
+//                        }
+//                        //if(entriesToDelete != null && entriesToDelete.Count > 0){
+//                        //    foreach(var entry in entriesToDelete) {
+//                        //        try {
+//                        //            entry.Delete();
+//                        //        }
+//                        //        catch(Exception ex){
+//                        //            Log.LogMessage(MessageImportance.Low, "Unable to delete entry from zip. {0}", ex.ToString());
+//                        //        }
+//                        //    }
+//                        //}
+//                        ZipFileExtensions.CreateEntryFromFile(zip, filePath, relpath, level);
+//                    }
+//                }
+//            }
+//            catch(Exception ex){
+//                Log.LogError(ex.ToString());
+//                return false;
+//            }
 
-        private CompressionLevel GetCompressionLevel() {
-            CompressionLevel level = CompressionLevel.Optimal;
-            try {
-                CompressionLevel result = (CompressionLevel) Enum.Parse(typeof(CompressionLevel), Compression);
-                level = result;
-            }
-            catch(Exception ex) {
-                Log.LogWarning("Unable to parse compression level [{0}]. Error [{1}]", Compression, ex.ToString());
-            }
-            return level;
-        }
-    }
-}
+//            return true;
+//        }
+
+//        private CompressionLevel GetCompressionLevel() {
+//            CompressionLevel level = CompressionLevel.Optimal;
+//            try {
+//                CompressionLevel result = (CompressionLevel) Enum.Parse(typeof(CompressionLevel), Compression);
+//                level = result;
+//            }
+//            catch(Exception ex) {
+//                Log.LogWarning("Unable to parse compression level [{0}]. Error [{1}]", Compression, ex.ToString());
+//            }
+//            return level;
+//        }
+//    }
+//}
